76f7f86a0c42de525f16a7bb79cf8bcea2ae956e
Thai Vang <thaivangxd@gmail.com>
2019-04-29 15:49:44 -0700

    add js file for calculating total and update visuals for crust cards

--- js/pizza_drill_basic.js
+++ js/pizza_drill_basic.js
@@ -1,173 +1,159 @@
-// Get the pizza size price and add it to the running Total
-// then pass that running total to the next function so 
-// the next function will add a particular total to the running total and so on...
-//
-// Keep doing this until you get all items added to the running total.
-//
-// Just remember that the syntax will be text1 = text1 + something + "<br>";
-// So you take the orginal value and concetenate to something new and end it with
-// an HTML line break so our code will write the next thing one line below instead
-// of overwriting the previous print out.
 
-/* NOTE: The comments below are for me to learn and understand how each function works 
-in realation to one another */
+var total = 0;
+var subtotal = " ";
+var pizzaSize = " ";
+subTotal = 0;
+document.getElementById("cart").style.display = "none";
 
+function getSize() {
+    if (document.getElementById("personal").checked) {
+        total += 6;
+        subtotal = total;
+        pizzaSize = "Personal";
+    } else if (document.getElementById("medium").checked) {       
+        total += 10;
+        subtotal = total;
+        pizzaSize = "Medium";
+    } else if (document.getElementById("large").checked) {
+        total += 14;
+        subtotal = total;
+        pizzaSize = "Large";
+    } else if (document.getElementById("extralarge").checked) {
+        total += 16;
+        subtotal = total;
+        pizzaSize = "Extra Large";
+    }
+};
+
+var meatChecked = [];
+var meatItem = document.getElementsByClassName("meats");
+var countI = 0;
+var x = 0;
+//how to clear the counter
+function getMeat(a, b) {
+    var i;
+    for (i = 0; i < b.length; ++i) { //if true counter increments by 1
+        if (b[i].checked) {
+            a.push(b[i].value);
+            countI = ++x;   
+        }  
+    }
+
+    if (a.length === 1) {
+        total += countI - 1;
+    } else if (a.length >= 2) {
+        countI -= 1;
+        total += countI;
+        subtotal = total;
+    } else {
+        total;
+    }
+
+    if (countI >= 1) {
+        countI = 0;
+    }
+};
+
+var sauce = " ";
+
+function getSauce() {
+    if (document.getElementById("flavor1").checked) {
+        sauce = "Marinara Sauce";
+    } else if (document.getElementById('flavor2').checked) {
+        sauce = "White Sauce";       
+    } else if (document.getElementById("flavor3").checked) {       
+        sauce = "Barbecue Sauce";
+    } else if (document.getElementById("flavor4").checked) {     
+        sauce = "No Sauce";
+    }
+};
+
+var cheese = " ";
+
+function getCheese() {
+    if (document.getElementById("flavor1").checked) {
+        cheese = "No Cheese";        
+    } else if (document.getElementById("flavor2").checked) {
+        cheese = "Regular";
+    } else if (document.getElementById("flavor3").checked) {
+        cheese = "Extra Cheese";
+    }
+};
+
+
+//function getMeat() {
+//    var i;
+//    for (i = 0; i < meatItem.length; ++i) { //if true counter increments by 1
+//        if (meatItem[i].checked) {
+//            meatChecked.push(meatItem[i].value);
+//            countI = ++x;
+//        }
+//    }
+
+//    if (meatChecked.length === 1) {
+//        total += countI - 1;
+//    } else if (meatChecked.length >= 2) {
+//        countI -= 1;
+//        total += countI;
+//        subtotal = total;
+//    } else {
+//        total;
+//    }
+//};
+
+var crust = " ";
+
+function getCrust() {
+    if (document.getElementById("crust1").checked) {
+        crust = "Plain";
+    } else if (document.getElementById("crust2").checked) {
+        crust = "Cheese Stuffed";
+    } else if (document.getElementById("crust3").checked) {
+        crust = "Garlic Butter";
+    } else if (document.getElementById("crust4").checked) {
+        crust = "Spicy";
+    } else if (document.getElementById("crust5").checked) {
+        crust = "Cinnamon";
+    }
+};
+
+function subtotal() {
+    document.innerHTML.getElementById("showtext2") = total;
+};
 
 function getReceipt() {
-	text1 = ""; // These initialize as empty strings but get passed from  
-	text2 = ""; // function to function, growing line by line into a full receipt
-	var runningTotal = 0; //value is zero when order is clicked
-	var sizeTotal = 0;
-	var sizeArray = document.getElementsByClassName("size"); // gathers all class names totaling 4 
-	for (var i = 0; i < sizeArray.length; i++) { //goes through each array of 4 
-		if (sizeArray[i].checked) { //checks if the size is selected
-			var selectedSize = sizeArray[i].value; //takes value from input
-			text1 = text1+selectedSize+"<br>"; // empty text1 = selected size then breaks to new line
-		}
-	}
-	if (selectedSize === "Personal Pizza") {
-		sizeTotal = 6;
-		text2 = text2+sizeTotal+"<br>";
-	} else if (selectedSize === "Medium Pizza") {
-		sizeTotal = 10;
-		text2 = text2+sizeTotal+"<br>";
-	} else if (selectedSize === "Large Pizza") {
-		sizeTotal = 14;
-		text2 = text2+sizeTotal+"<br>";
-	} else if (selectedSize === "Extra Large Pizza") {
-		sizeTotal = 16;
-		text2 = text2+sizeTotal+"<br>";
-	}
-	runningTotal = sizeTotal;
-	getMeat(runningTotal,text1,text2); // All three of these variables will be passed on to each function
-};	//rt = 6, t1 = personal, t2 = 6 
+    getSize();
+    getMeat(meatChecked, meatItem);
+    getSauce();
+    getCheese();
+    getCrust();
 
-// With both the meat and veggie functions each item in the array will be
-// 1 dollar but the first is going to be free so we can count the total
-// of items in their array and subtract 1 to get the total item cost
-//
-// Now we can add the item cost to our running total to get the new
-// running total and then pass this new running total to the next function
-// Just keep up this process until we've added all items to the running total
-function getMeat(runningTotal,text1,text2) {
-	var runningTotal = runningTotal;
-	var meatTotal = 0;
-	var selectedMeat = []; //empty array 
-	var meatArray = document.getElementsByClassName("meats");
-	for (var j = 0; j < meatArray.length; j++) {
-		if (meatArray[j].checked) {
-			selectedMeat.push(meatArray[j].value); // adds value of selected meats to an empty array selectedMeat
-		}
-	}
-	var meatCount = selectedMeat.length; //Variable takes on length of array which is 1
-	if (meatCount > 1) { 
-		meatTotal = (meatCount - 1);
-	} else {
-		meatTotal = 0;
-	}
-	runningTotal = (runningTotal + meatTotal); //runningtotal of 6 + meatotal of 0 
-	for (var j = 0; j < selectedMeat.length; j++) {
-			text1 = text1+selectedMeat[j]+"<br>"; // t1 "" + sM pepperoni
-			if (meatCount <= 1) {
-				text2 = text2 + 0 + "<br>"; // t2 = "" + 0
-				meatCount = meatCount - 1;
-			} else if (meatCount == 2) {
-				text2 = text2 + 1 + "<br>";
-				meatCount = meatCount - 1;
-			} else {
-				text2 = text2 + 1 + "<br>";
-				meatCount = meatCount - 1;
-			}
-	}
-	getVeggie(runningTotal,text1,text2);
+    var cart = document.getElementById("cart");
+    if (cart.style.display === "none") {
+        cart.style.opacity = 1;
+        cart.style.display = "block";
+    }
+    //selected or not 
+    //if () {
+
+    //} else {
+    //    document.getElementById('showText1').innerHTML = pizzaSize + "<br>" + meatChecked.join(" ") + " " + sauce;
+    //}
+    document.getElementById("showText1").innerHTML = pizzaSize + "<br>" + meatChecked.join("<br>") + "<br>" + sauce + "<br>" + cheese + "<br>" + "<br>" + crust;
+    document.getElementById("totalPrice2").innerHTML = "$" + total;
 };
 
-function getVeggie(runningTotal,text1,text2) {
-	var veggieTotal = 0;
-	var selectedVeggie = [];
-	var veggieArray = document.getElementsByClassName("veggies");
-	for (var j = 0; j < veggieArray.length; j++) {
-		if (veggieArray[j].checked) {
-			selectedVeggie.push(veggieArray[j].value);
-		}
-	}
-	var veggieCount = selectedVeggie.length;
-	if (veggieCount >= 2) {
-		veggieTotal = (veggieCount - 1);
-	} else {
-		veggieTotal = 0;
-	}
-	runningTotal = (runningTotal + veggieTotal);
-	for (var j = 0; j < selectedVeggie.length; j++) {
-			text1 = text1+selectedVeggie[j]+"<br>";
-			if (veggieCount <= 1) {
-				text2 = text2 + 0 + "<br>";
-				veggieCount = veggieCount - 1;
-			} else if (veggieCount == 2) {
-				text2 = text2 + 1 + "<br>";
-				veggieCount = veggieCount - 1;
-			} else {
-				text2 = text2 + 1 + "<br>";
-				veggieCount = veggieCount - 1;
-			}
-	}
-	getCheese(runningTotal,text1,text2);
-};
-
-function getCheese(runningTotal,text1,text2) {
-	var cheeseTotal = 0;
-	var selectedCheese = [];
-	var cheeseArray = document.getElementsByClassName("cheese");
-	for (var j = 0; j < cheeseArray.length; j++) {
-		if (cheeseArray[j].checked) {
-			selectedCheese = cheeseArray[j].value;
-		}
-		if (selectedCheese === "Extra cheese") {
-			cheeseTotal = 3;
-		}
-	}
-	text2 = text2+cheeseTotal+"<br>";
-	text1 = text1+selectedCheese+"<br>";
-	runningTotal = (runningTotal + cheeseTotal);
-	getSauce(runningTotal,text1,text2);
-};
-
-function getSauce(runningTotal,text1,text2) {
-	var sauceArray = document.getElementsByClassName("sauce");
-	for (var j = 0; j < sauceArray.length; j++) {
-		if (sauceArray[j].checked) {
-			selectedSauce = sauceArray[j].value;
-			text1 = text1 + selectedSauce +"<br>";
-		}
-	}
-	text2 = text2 + 0 + "<br>";
-	getCrust(runningTotal,text1,text2)
-};
-
-function getCrust(runningTotal,text1,text2) {
-	var crustTotal = 0;
-	var selectedCrust;
-	var crustArray = document.getElementsByClassName("crust");
-	for (var j = 0; j < crustArray.length; j++) {
-		if (crustArray[j].checked) {
-			selectedCrust = crustArray[j].value;
-			text1 = text1 + selectedCrust + "<br>";
-		}
-		if (selectedCrust === "Cheese Stuffed Crust") {
-			crustTotal = 3;
-		}
-	}
-	runningTotal = (runningTotal + crustTotal);
-	text2 = text2 + crustTotal + "<br>";
-	document.getElementById("cart").style.opacity=1;
-	document.getElementById("showText1").innerHTML=text1;
-	document.getElementById("showText2").innerHTML=text2;
-	document.getElementById("totalPrice2").innerHTML = "</h3>$"+runningTotal+".00"+"</h3>";
-};
-
-// This code clears the form selections to their defaults and then sets the 
-// corresponding div's CSS opacity to 0, effectively hiding it from view.
 function clearAll() {
-	document.getElementById("frmMenu").reset();
-	document.getElementById("cart").style.opacity=0;
+    var clear = document.getElementById("cart");
+    
+    if (clear.style.display === "block") {
+        clear.style.opacity = 0;
+        clear.style.display = "none";
+        meatChecked.length = 0;
+        total = 0;
+        countI = 0;
+        //document.getElementById("totalPrice2").innerHTML = "$" + total;
+    }
 };
+
+
